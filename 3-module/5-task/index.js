3413c166a41ad64e05930e0b311c672b4862c26d
viktorianet <viktorianet@ya.ru>
2026-03-16 20:53:06 +0300

    Занятие 3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+
+  let arr = str.split(" ");
+
+  let numbers = arr.filter(item => !isNaN(item))
+                   .map(item => Number(item));
+
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+  };
 }
