35a0eb01a478f230186dcf6c2be0350cc3ddeee9
viktorianet <viktorianet@ya.ru>
2026-04-01 13:01:00 +0300

    Занятие 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+
+  ul.innerHTML = friends
+    .map(friend => `<li>${friend.firstName} ${friend.lastName}</li>`)
+    .join('');
+
+  return ul;
 }
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,10 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let rows = table.rows;
+
+  for (let i = 0; i < rows.length; i++) {
+    let cell = rows[i].cells[i];
+    if (cell) {
+      cell.style.backgroundColor = 'red';
+    }
+  }
 }
